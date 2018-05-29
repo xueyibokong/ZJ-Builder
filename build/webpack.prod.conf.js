09898ee9857b748308beddd92fe9e78c0df79841
张军 <819396183@qq.com>
2018-05-29 11:22:00 +0800

    修改

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -3,7 +3,7 @@ var
     Merge = require('webpack-merge'),
     webpackBaseConf = require('./webpack.base.conf'),
     config = require('./config'),
-    CssPbulicPathReset = require('./css-publicpath-reset')
+    CssPbulicPathReset = require('webpack-css-publicpath-reset')
 
 module.exports = Merge(webpackBaseConf,{
     devtool: config.prod.jsSourceMap && 'cheap-module-eval-source-map',
@@ -23,6 +23,7 @@ module.exports = Merge(webpackBaseConf,{
             minimize: true
         }),
         new CssPbulicPathReset({
+            includeLink : config.prod.outputPublicPath.js,
             publicPath : config.prod.outputPublicPath.css
         }),
         /*---------- 使用了 NoEmitOnErrorsPlugin 后页面中的报错不会阻塞，但是会在编译结束后报错  ----------*/
